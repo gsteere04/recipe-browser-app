eac79ae28ac0e18cd10022c4c311de7bfb5f6ba8
Grant <gsteere4717@xmail.dixietech.edu>
2024-08-06 09:51:58 -0600

    add RecipeList.tsx component

---
+++ frontend/recipe-browser/src/components/RecipeList.tsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import api from '../services/api';
+import Recipe from './Recipe';
+
+const RecipeList: Recipe.FC = () => {
+    const[recipes, setRecipes] = useState([]);
+
+    useEffect(() => {
+        const fetchRecipies = async () => {
+            const response = await api.get('/recipes/');
+            setRecipes(response.data);
+        };
+        fetchRecipies();
+    }, []);
+    return (
+        <div>
+            {recipes.map((recipe: any) => (
+                <Recipe key={recipe.id} recipe={recipe} />
+            ))}
+        </div>
+    );
+};
+
+export default RecipeList
--- frontend/recipe-browser/src/services/api.ts
+++ frontend/recipe-browser/src/services/api.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https//localhost:'
-})
+    baseURL: 'https//localhost:5432',
+});
+
+export default api
