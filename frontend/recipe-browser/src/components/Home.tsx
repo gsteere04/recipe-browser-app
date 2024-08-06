eca8283e40f87c0e93320410d1e11c755450e86b
Grant <gsteere4717@xmail.dixietech.edu>
2024-08-06 11:29:52 -0600

    add home component

---
+++ frontend/recipe-browser/src/components/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import RecipeList from '../components/RecipeList';
+
+const Home: React.FC = () => (
+    <div>
+        <h1>Recipe Browser</h1>
+        <RecipeList />
+    </div>
+);
+
+export default Home;
